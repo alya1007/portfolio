114d407da0081c9be274eb0c226c6c1a16464c2b
alya1007 <alexandra.konjevic@isa.utm.md>
2023-05-06 18:08:32 +0300

    skills carousel

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,19 +1,66 @@
 import React from "react";
 import CarouselItem from "./CarouselItem";
-import { TbBrandJavascript, TbBrandHtml5, TbBrandCss3 } from "react-icons/tb";
+import {
+	TbBrandJavascript,
+	TbBrandHtml5,
+	TbBrandCss3,
+	TbBrandReact,
+	TbBrandTypescript,
+	TbBrandTailwind,
+	TbBrandRedux,
+	TbBrandBootstrap,
+} from "react-icons/tb";
+import { SiAntdesign, SiJquery } from "react-icons/si";
 
 function Carousel() {
+	const items = [
+		{
+			itemIcon: <TbBrandJavascript className="carousel-item__icon" />,
+			title: "JavaScript",
+		},
+		{
+			itemIcon: <TbBrandHtml5 className="carousel-item__icon" />,
+			title: "HTML5",
+		},
+		{
+			itemIcon: <TbBrandCss3 className="carousel-item__icon" />,
+			title: "CSS3",
+		},
+		{
+			itemIcon: <TbBrandReact className="carousel-item__icon" />,
+			title: "React",
+		},
+		{
+			itemIcon: <TbBrandTypescript className="carousel-item__icon" />,
+			title: "TypeScript",
+		},
+		{
+			itemIcon: <TbBrandTailwind className="carousel-item__icon" />,
+			title: "TailwindCSS",
+		},
+		{
+			itemIcon: <TbBrandRedux className="carousel-item__icon" />,
+			title: "Redux",
+		},
+		{
+			itemIcon: <TbBrandBootstrap className="carousel-item__icon" />,
+			title: "Bootstrap",
+		},
+		{
+			itemIcon: <SiAntdesign className="carousel-item__icon" />,
+			title: "Ant Design",
+		},
+		{
+			itemIcon: <SiJquery className="carousel-item__icon" />,
+			title: "jQuery",
+		},
+	];
+
 	return (
 		<div className="carousel">
-			<CarouselItem
-				itemIcon={<TbBrandJavascript className="carousel-item__icon" />}
-			/>
-			<CarouselItem
-				itemIcon={<TbBrandHtml5 className="carousel-item__icon" />}
-			/>
-			<CarouselItem
-				itemIcon={<TbBrandCss3 className="carousel-item__icon" />}
-			/>
+			{items.map((item, index) => (
+				<CarouselItem key={index} itemIcon={item.itemIcon} title={item.title} />
+			))}
 		</div>
 	);
 }
--- src/Components/CarouselItem.jsx
+++ src/Components/CarouselItem.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-function CarouselItem({ itemIcon }) {
-	return <div className="carousel-item">{itemIcon}</div>;
+function CarouselItem({ itemIcon, title }) {
+	return (
+		<div className="carousel-item">
+			{itemIcon}
+			<h3 className="carousel-item__title">{title}</h3>
+		</div>
+	);
 }
 
 export default CarouselItem;
